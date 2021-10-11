40e4df389cf1f26023167be820cb5bf20b987b9b
Andrii Salii <tehnicyss@gmail.com>
2021-10-11 13:00:02 +0300

    Added a few more questions about peoples

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -83,38 +83,94 @@ export const QUESTIONS: Question[] = [
 
 export const PEOPLE_QUESTIONS: Question[] = [
     {
-        text: 'Question 1?', answers: [
+        text: 'Чи полюбляєте ви гуляти?', answers: [
             { text: 'Рідко', value: -1 },
             { text: 'Іноді', value: 0 },
             { text: 'Часто', value: 1 },
         ]
     },
     {
-        text: 'Question 2?', answers: [
-            { text: 'Ні', value: -1 },
-            { text: 'Не впевнений', value: 0 },
-            { text: 'Так', value: 1 },
-        ]
-    },
-    {
-        text: 'Question 3?', answers: [
-            { text: 'Так', value: -1 },
-            { text: 'Можливо', value: 0 },
+        text: 'Захоплюєтесь спортом?', answers: [
+            { text: 'Так, активно займаюся певним видом', value: -1 },
+            { text: 'Люблю спостерігати', value: 0 },
             { text: 'Ні', value: 1 },
         ]
     },
     {
-        text: 'Question 4?', answers: [
-            { text: 'Майже ніколи', value: -1 },
-            { text: 'Іноді', value: 0 },
-            { text: 'Регулярно', value: 1 },
+        text: 'Чи любите ви читати?', answers: [
+            { text: 'Ні', value: -1 },
+            { text: 'Рідко', value: 0 },
+            { text: 'Обожнюю', value: 1 },
         ]
     },
     {
-        text: 'Question 5?', answers: [
+        text: 'Чи подобається вам готувати', answers: [
+            { text: 'Ні', value: -1 },
+            { text: 'Іноді, коли є вільний час', value: 0 },
+            { text: 'Так', value: 1 },
+        ]
+    },
+    {
+        text: 'Чи готові ви завести домашню тварину?', answers: [
             { text: 'Ні', value: -1 },
             { text: 'Не впевнений', value: 0 },
-            { text: 'Так', value: 1 },
+            { text: 'Так, з радістю', value: 1 },
+        ]
+    },
+    {
+        text: 'Ви "Сова" чи "Жайворонок"?', answers: [
+            { text: 'Сова', value: -1 },
+            { text: 'Не впевнений', value: 0 },
+            { text: 'Жайворонок', value: 1 },
+        ]
+    },
+    {
+        text: 'Чай чи кава?', answers: [
+            { text: 'Чай', value: -1 },
+            { text: 'Склянка води', value: 0 },
+            { text: 'Кава', value: 1 },
+        ]
+    },
+    {
+        text: 'Активно відпочити в горах чи ліниво полежати на пляжі?', answers: [
+            { text: 'Активний відпочинок', value: -1 },
+            { text: 'Полюбляю обидва варіанти', value: 0 },
+            { text: 'Ледачий відпочинок', value: 1 },
+        ]
+    },
+    {
+        text: 'Як часто ви дивитися фільми чи серіали?', answers: [
+            { text: 'Майже дивлюся', value: -1 },
+            { text: 'Годинку-дві на вихідних', value: 0 },
+            { text: 'Дуже часто', value: 1 },
+        ]
+    },
+    {
+        text: 'Фільм чи книга?', answers: [
+            { text: 'Фільм', value: -1 },
+            { text: 'Не впевнений', value: 0 },
+            { text: 'Книга', value: 1 },
+        ]
+    },
+    {
+        text: 'Коти чи Собаки?', answers: [
+            { text: 'Коти', value: -1 },
+            { text: 'Не можу визначитися', value: 0 },
+            { text: 'Собаки', value: 1 },
+        ]
+    },
+    {
+        text: 'Яка музика вам більше по душі?', answers: [
+            { text: 'Рок, хіп-хоп', value: -1 },
+            { text: 'Поп, інді, танцювальна', value: 0 },
+            { text: 'Джаз, інструментальна', value: 1 },
+        ]
+    },
+    {
+        text: 'Яка кухня більше до вподоби?', answers: [
+            { text: 'Класична Європейська', value: -1 },
+            { text: 'Середземноморська', value: 0 },
+            { text: 'Східна кухня', value: 1 },
         ]
     },
 ];
